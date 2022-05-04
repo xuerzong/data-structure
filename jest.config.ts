9c32ba4dd6862cf8c3c07475badd1ae133a3bcce
xuerzong <xuerzong@gmail.com>
2022-05-04 22:34:13 +0800

    feat: v0.0.1

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
+};
---
+++ src/demo/index.test.ts
@@ -0,0 +1,7 @@
+import { add } from '.';
+
+describe('demo.add', () => {
+  it('it is true', () => {
+    expect(add(1, 2)).toBe(3);
+  })
+})
