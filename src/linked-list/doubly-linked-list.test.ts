6736a27f63f24e2fe7eac7d27d8cd27de1370874
xucong <xuerzong@gmail.com>
2022-07-04 12:19:40 +0800

    fix(DoublyLinkedList): insert-update-remove by index

--- src/linked-list/doubly-linked-list.test.ts
+++ src/linked-list/doubly-linked-list.test.ts
@@ -1,37 +1,92 @@
 import DoublyLinkedList, { DoublyLinkedNode } from './doubly-linked-list'
 
-const linked = new DoublyLinkedList()
+let linked: DoublyLinkedList<number>
 
 describe('LinkedList.DoublyLinkedList', () => {
+  beforeEach(() => {
+    linked = new DoublyLinkedList()
+  })
   it('insert: should be the new node', () => {
     const tmpNode1 = new DoublyLinkedNode(0)
     const tmpNode2 = new DoublyLinkedNode(1)
     linked.insert(0)
+    expect(linked.len()).toBe(1)
     expect(linked.head).toEqual(tmpNode1)
 
     linked.insert(1)
     tmpNode1.prev = tmpNode2
     tmpNode2.next = tmpNode1
+    expect(linked.len()).toBe(2)
     expect(linked.head?.next).toEqual(tmpNode1)
   })
 
-  it('search: should return the node which key is equal input key', () => {
-    const tmpNode1 = new DoublyLinkedNode(0)
-    const tmpNode2 = new DoublyLinkedNode(1)
-    tmpNode1.prev = tmpNode2
-    tmpNode2.next = tmpNode1
-    expect(linked.search(1)).toEqual(tmpNode2)
-    expect(linked.search(0)).toEqual(tmpNode1)
-    expect(linked.search(2)).toBe(null)
-  })
-  it('delete: should remove the node which key is equal input key', () => {
-    const tmpNode1 = new DoublyLinkedNode(0)
-    const tmpNode2 = new DoublyLinkedNode(1)
-    linked.remove(1)
-    expect(linked.head).toEqual(tmpNode1)
-
+  it('toArray: should return the key list', () => {
+    linked.insert(0)
     linked.insert(1)
-    linked.remove(0)
-    expect(linked.head).toEqual(tmpNode2)
+    expect(linked.toArray()).toEqual([1, 0])
+
+    linked.insert(2)
+    expect(linked.toArray()).toEqual([2, 1, 0])
+  })
+
+  it('insertByIndex: should insert new node at index', () => {
+    linked.insertByIndex(0, 0)
+    linked.insertByIndex(1, 1)
+    expect(linked.len()).toBe(2)
+    expect(linked.toArray()).toEqual([0, 1])
+
+    linked.insertByIndex(1, 2)
+    expect(linked.len()).toBe(3)
+    expect(linked.toArray()).toEqual([0, 2, 1])
+  })
+
+  it('insertFromHead: should insert new node at first index', () => {
+    linked.insertFromHead(0)
+    linked.insertFromHead(1)
+    expect(linked.len()).toBe(2)
+    expect(linked.toArray()).toEqual([1, 0])
+
+    linked.insertFromHead(2)
+    expect(linked.len()).toBe(3)
+    expect(linked.toArray()).toEqual([2, 1, 0])
+  })
+
+  it('insertFromTail: should insert new node at last index', () => {
+    linked.insertFromTail(0)
+    linked.insertFromTail(1)
+    expect(linked.len()).toBe(2)
+    expect(linked.toArray()).toEqual([0, 1])
+
+    linked.insertFromTail(2)
+    expect(linked.len()).toBe(3)
+    expect(linked.toArray()).toEqual([0, 1, 2])
+  })
+
+  it('findByIndex: should return the key at index', () => {
+    linked.insert(0)
+    linked.insert(1)
+    expect(linked.findByIndex(0)?.value).toEqual(1)
+    expect(linked.findByIndex(1)?.prev?.value).toEqual(1)
+
+    expect(linked.findByIndex(-1)).toBe(null)
+    expect(linked.findByIndex(linked.len() + 1)).toBe(null)
+  })
+
+  it('updateByIndex: should update the key at index', () => {
+    linked.insert(0)
+    linked.insert(1)
+    linked.updateByIndex(0, 4)
+    linked.updateByIndex(1, 3)
+    expect(linked.toArray()).toEqual([4, 3])
+  })
+
+  it('removeByIndex: should remove the key at index', () => {
+    linked.insert(0)
+    linked.insert(1)
+    linked.insert(2)
+
+    linked.removeByIndex(1)
+    expect(linked.len()).toBe(2)
+    expect(linked.toArray()).toEqual([2, 0])
   })
 })
--- src/linked-list/doubly-linked-list.ts
+++ src/linked-list/doubly-linked-list.ts
@@ -1,63 +1,144 @@
 import { isNull, isNotNull } from '@/_utils/is'
 
-export class DoublyLinkedNode {
-  key: number | null
-  prev: DoublyLinkedNode | null
-  next: DoublyLinkedNode | null
-  constructor(key: number) {
-    this.key = key
+export class DoublyLinkedNode<T> {
+  value: T
+  prev: DoublyLinkedNode<T> | null
+  next: DoublyLinkedNode<T> | null
+  constructor(key: T) {
+    this.value = key
     this.prev = null
     this.next = null
   }
 }
 
-interface IDoublyLinkedList {
-  insert: (key: number) => void
+interface IDoublyLinkedList<T> {
+  len(): number
 
-  search: (key: number) => DoublyLinkedNode | null
+  insert(value: T): void
 
-  remove: (key: number) => void
+  insertByIndex(index: number, value: T): void
+
+  insertFromHead(value: T): void
+
+  insertFromTail(value: T): void
+
+  findByIndex: (index: number) => DoublyLinkedNode<T> | null
+
+  updateByIndex(index: number, value: T): void
+
+  removeByIndex(index: number): void
+
+  toArray(): T[]
 }
 
-export default class DoublyLinkedList implements IDoublyLinkedList {
-  head: DoublyLinkedNode | null
+export default class DoublyLinkedList<T = any> implements IDoublyLinkedList<T> {
+  head: DoublyLinkedNode<T> | null
+  private length: number = 0
 
   constructor() {
     this.head = null
   }
 
-  insert(key: number) {
-    const newNode = new DoublyLinkedNode(key)
+  len() {
+    return this.length
+  }
+
+  insert(value: T) {
+    this.length = this.length + 1
+    const newNode = new DoublyLinkedNode(value)
+
     newNode.next = this.head
-    if (isNotNull<DoublyLinkedNode>(this.head)) {
+    if (isNotNull<DoublyLinkedNode<T>>(this.head)) {
       this.head.prev = newNode
     }
+
     this.head = newNode
   }
 
-  search(key: number) {
+  insertByIndex(index: number, value: T) {
+    if (this.length === 0) {
+      return this.insertFromHead(value)
+    }
+
+    if (this.length > this.length) {
+      // TODO optimize
+      return this.insertFromTail(value)
+    }
+
+    this.length = this.length + 1
+
+    const preNode = this.findByIndex(index - 1) as DoublyLinkedNode<T>
+    const newNode = new DoublyLinkedNode(value)
+
+    newNode.next = preNode.next
+
+    preNode.next = newNode
+    newNode.prev = preNode
+  }
+
+  insertFromHead(value: T): void {
+    this.insert(value)
+  }
+
+  insertFromTail(value: T): void {
+    this.insertByIndex(this.length, value)
+  }
+
+  findByIndex(index: number) {
     let headCache = this.head
-    while (isNotNull<DoublyLinkedNode>(headCache) && headCache.key !== key) {
-      headCache = headCache.next
+
+    const oversize = index < 0 || index > this.length
+    if (oversize) {
+      return null
+    }
+
+    let i = 0
+    while (i++ < index) {
+      headCache = headCache!.next
     }
     return headCache
   }
 
-  remove(key: number) {
-    if (isNull(this.head)) {
+  updateByIndex(index: number, value: T) {
+    const curNode = this.findByIndex(index)
+    if (isNull(curNode)) {
+      // pass
+    } else {
+      curNode.value = value
+    }
+  }
+
+  removeByIndex(index: number) {
+    const curNode = this.findByIndex(index)
+
+    if (isNull(curNode)) {
       return
     }
-    const curNode = this.search(key)
-    if (isNotNull<DoublyLinkedNode>(curNode)) {
-      if (isNotNull<DoublyLinkedNode>(curNode.prev)) {
-        curNode.prev.next = curNode.next
-      } else {
-        this.head = curNode.next
-      }
 
-      if (isNotNull<DoublyLinkedNode>(curNode.next)) {
-        curNode.next.prev = curNode.prev
-      }
+    this.length = this.length - 1
+
+    if (isNull(curNode.prev)) {
+      // current node is head node
+      this.head = curNode.next
+    } else {
+      curNode.prev.next = curNode.next
     }
+
+    if (isNotNull<DoublyLinkedNode<T>>(curNode.next)) {
+      curNode.next.prev = curNode.prev
+    }
+  }
+
+  toArray() {
+    const result: T[] = []
+
+    let headCache = this.head
+
+    while (headCache) {
+      result.push(headCache.value)
+      headCache = headCache.next
+    }
+
+    return result
   }
 }
